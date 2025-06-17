1f467d058c2efa743bf18add244aa85054f485f4
JuanFer <juanfernandog824@gmail.com>
2025-06-17 08:43:14 -0500

    Version 001

---
+++ carol/bin/carol.dart
@@ -0,0 +1,5 @@
+import 'package:carol/carol.dart' as carol;
+
+void main(List<String> arguments) {
+  print('Hello world: ${carol.calculate()}!');
+}
---
+++ carol/test/carol_test.dart
@@ -0,0 +1,8 @@
+import 'package:carol/carol.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
